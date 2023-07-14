1a539442c98fb334c7c4146707bfc2d15731c3e0
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2023-07-13 23:34:47 -0400

    Cool dictionary algo for swift.

--- TestApp/TestApp/main.swift
+++ TestApp/TestApp/main.swift
@@ -7,19 +7,31 @@
 
 import Foundation
 
-var number: Int = Int.random(in: 0..<9);
-
 var number_count: [Int : Int] = [:];
 
 var minCount : Int = 0;
-var maxCount : Int = Int(10e4);
+var maxCount : Int = Int(10e5);
+var totalCount : Int = maxCount - minCount;
 
-for number in minCount..<maxCount {
+for _ in minCount..<maxCount {
     let randomNumber = Int.random(in: 0..<10);
-    if number_count.contains(where: {randomNumber in number_count.keys}) {
-        number_count.updateValue(number_count[randomNumber] + 1, forKey: randomNumber);
+    if let count = number_count[randomNumber] {
+        number_count.updateValue(count + 1, forKey: randomNumber);
     }
-    else{
+    else {
         number_count[randomNumber] = 1;
     }
+}
+
+var number_count_2: [Int : Double] = [:];
+
+for (key, value) in number_count {
+    let updatedValue: Double = Double(value) / Double(totalCount);
+    number_count_2[key] = updatedValue;
+}
+
+
+
+for (key, value) in number_count_2 {
+    print("random number: \(key), percent: \(value * 100)%");
 }
